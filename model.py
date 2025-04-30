dd97a25dd51f3f5ef35ff1f4465fef2dba5acaa2
Igor Novais <igor.novais.b@gmail.com>
2025-04-30 08:41:47 -0300

    Commit 10: Replacing conditional with polymorphism - part 3

--- model.py
+++ model.py
@@ -21,26 +21,6 @@ class Book:
     def get_frequent_renter_points(self, days_rented: int):
         return self.price.get_frequent_renter_points(days_rented)
     
-    # def get_charge(self, days_rented) -> float:
-    #     amount = 0
-    #     if self.price_code == Book.REGULAR:
-    #         amount += 2
-    #         if days_rented > 2:
-    #             amount += (days_rented - 2) * 1.5
-    #     elif self.price_code == Book.NEW_RELEASE:
-    #         amount += days_rented * 3
-    #     elif self.price_code == Book.CHILDREN:
-    #         amount += 1.5
-    #         if days_rented > 3:
-    #             amount += (days_rented - 3) * 1.5
-    #     return amount
-    
-    # def get_frequent_renter_points(self, days_rented):
-    #     points = 1
-    #     if self.price_code == Book.NEW_RELEASE and days_rented > 1:
-    #         points += 1
-    #     return points
-    
 class Price:
 
     def get_charge(self, days_rented: int) -> float:
@@ -50,13 +30,37 @@ class Price:
         pass
 
 class RegulaPrice(Price):
-    pass
+    
+    def get_charge(self, days_rented: int) -> float:
+        amount = 2
+        if days_rented > 2:
+            amount += (days_rented - 2) * 1.5
+        return amount
+    
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        return 1
 
 class NewReleasePrice(Price):
-    pass
+    
+    def get_charge(self, days_rented: int) -> float:
+        return days_rented * 3
+    
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        points = 1
+        if days_rented > 1:
+            points += 1
+        return points
 
 class ChildrenPrice(Price):
-    pass
+        
+    def get_charge(self, days_rented: int) -> float:
+        amount = 1.5
+        if days_rented > 3:
+            amount += (days_rented - 3) * 1.5
+        return amount
+    
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        return 1
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
